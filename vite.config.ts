5a40709e69f0347f1cfce85dd0a42b35811cc0f6
tomo-academy <tomoacademyofficial@gmail.com>
2025-10-01 23:47:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,12 +15,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  optimizeDeps: {
-    exclude: ['qrcode']
-  },
-  build: {
-    rollupOptions: {
-      external: ['qrcode']
-    }
-  }
 }));
